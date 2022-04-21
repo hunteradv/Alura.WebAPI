ee7b63308ceb72125eb3aaed774c0bebdc3acf68
HunterAdv <gustavopaulody@gmail.com>
2022-04-21 12:41:58 -0300

    Criando controller ReadListsController para disponibilizar get de listas na Api

---
+++ Alura.WebAPI.WebApp/Api/ReadListsController.cs
@@ -0,0 +1,47 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ReadList = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReadListsController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ReadListsController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        private ReadList CreateList(TipoListaLeitura type)
+        {
+            return new ReadList
+            {
+                Tipo = type.ParaString(),
+                Livros = _repo.All.Where(l => l.Lista == type).ToList()
+            };
+        }
+
+        [HttpGet]
+        public IActionResult AllLists()
+        {            
+            ReadList toRead = CreateList(TipoListaLeitura.ParaLer);
+            ReadList reading = CreateList(TipoListaLeitura.Lendo);
+            ReadList alreadyRead = CreateList(TipoListaLeitura.Lidos);
+            var collection = new List<ReadList> { toRead, reading, alreadyRead };
+            return Ok(collection);
+        }
+
+        [HttpGet("{type}")]
+        public IActionResult Recovery(TipoListaLeitura type)
+        {
+            var list = CreateList(type);
+            return Ok(list);
+        }
+    }
+}
