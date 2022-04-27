a2b7bc926cb0986ac5d718e229124b6225b4d92c
HunterAdv <gustavopaulody@gmail.com>
2022-04-27 17:48:14 -0300

    adicionando [authorize] para precisar estar autenticado para conseguir utilizar a api

---
+++ Alura.WebAPI.WebApp/Formatters/BookCsvFormatter.cs
@@ -0,0 +1,42 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Formatters
+{
+    public class BookCsvFormatter : TextOutputFormatter
+    {
+        public BookCsvFormatter()
+        {
+            var textCsvMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var appCsvMediaType = MediaTypeHeaderValue.Parse("application/csv");
+            SupportedMediaTypes.Add(textCsvMediaType);        
+            SupportedMediaTypes.Add(appCsvMediaType);
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(System.Type type)
+        {
+            return type == typeof(LivroApi);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {           
+            var bookInCsv = "";
+
+            if (context.Object is LivroApi)
+            {
+                var book = context.Object as LivroApi;
+
+                bookInCsv = $"/{book.Titulo};{book.Subtitulo};{book.Autor};{book.Lista}/";
+            }
+
+            using (var writer = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return writer.WriteAsync(bookInCsv);
+            }//writer.Close()                
+        }
+    }
+}
