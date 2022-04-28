562c09dba15f81b5de327efffbd42b51545aa66f
HunterAdv <gustavopaulody@gmail.com>
2022-04-28 17:59:40 -0300

    Adicionando metodo e controller para geração de token de autenticação

---
+++ Alura.WebAPI.WebApp/Api/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Alura.ListaLeitura.Seguranca;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public LoginController(SignInManager<Usuario> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Token(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Login, model.Password, true, true);
+
+                if (result.Succeeded)
+                {
+                    //cria token (header + payload >> direitos + signature)
+                    var rights = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, model.Login),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    };
+
+                    var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valide"));
+                    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        issuer: "Alura.WebApp",
+                        audience: "Postman",
+                        claims: rights,
+                        signingCredentials: credentials,
+                        expires: DateTime.Now.AddMinutes(30)
+                    );
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+                return Unauthorized(); //401
+            }
+            return BadRequest(); //400
+        }     
+    }
+}
